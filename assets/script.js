aa73489c0c454693606301012bde27921f1ae08c
Austin Miller <austin.miller1424@gmail.com>
2022-08-30 15:51:07 -0500

    added form for frist name and added to consloe for use on page 2

---
+++ assets/script.js
@@ -0,0 +1,14 @@
+var userInput = document.getElementById('firstName');
+
+//capture user first name
+document.querySelector('form.userForm').addEventListener('submit', function (e) {
+    e.preventDefault();
+    console.log(userInput.value);    
+});
+//send user to new html page
+
+//capture user last name 
+
+//send user back to homepage 
+
+
