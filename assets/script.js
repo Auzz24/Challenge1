735f78b5f325d118c63d2492f0d9dfd233bbd857
Austin Miller <austin.miller1424@gmail.com>
2022-08-30 15:57:17 -0500

    added winidow.open to new HTML page

--- assets/script.js
+++ assets/script.js
@@ -3,9 +3,11 @@ var userInput = document.getElementById('firstName');
 //capture user first name
 document.querySelector('form.userForm').addEventListener('submit', function (e) {
     e.preventDefault();
-    console.log(userInput.value);    
+    console.log(userInput.value);  
+    //send user to new html page
+    window.open('new.html')
+      
 });
-//send user to new html page
 
 //capture user last name 
 
