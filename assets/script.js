52ccc03e43f340db274e1b41864036ca2a3f7599
Austin Miller <austin.miller1424@gmail.com>
2022-08-31 13:17:16 -0500

    new.html closes when button is clicked, added user input to local storage

--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,7 @@ var userInput = document.getElementById('firstName');
 //capture user first name
 document.querySelector('form.userForm').addEventListener('submit', function (e) {
     e.preventDefault();
+    localStorage.setItem("userSubmit", userInput.value)
     console.log(userInput.value);  
     //send user to new html page
     let myWindow
