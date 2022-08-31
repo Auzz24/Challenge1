0a873ed2accce821252135d94408ca90e36af8c9
Austin Miller <austin.miller1424@gmail.com>
2022-08-31 12:55:13 -0500

    added button to return to homepage, still need to add input to local storage

--- assets/script.js
+++ assets/script.js
@@ -5,12 +5,30 @@ document.querySelector('form.userForm').addEventListener('submit', function (e) 
     e.preventDefault();
     console.log(userInput.value);  
     //send user to new html page
-    window.open('new.html')
-      
+    let myWindow
+    function openWindow(){
+    myWindow = window.open('new.html')
+    }
+    openWindow()
 });
 
 //capture user last name 
 
 //send user back to homepage 
+
+
+// function secondPage() {
+//     var returnButton = document.getElementById("submitButtonTwo");
+//     returnButton.addEventListener("click", returnFunction);
+
+//     function returnFunction(){
+//         window.close()
+//     }
+//     };
+
+
+
+// firstPage();
+// secondPage();
 
 
